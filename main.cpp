d98064f26d19bde1922d00bf948614604d3cdbe6
Brandvie <brandymbuyi66@gmail.com>
2026-03-05 19:14:54 +0000

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -32,7 +32,7 @@ int main()
     buttons.push_back(new Button(font, {660, 140}, {120, 30}, "Only Blue", [&images, &selectedImage](){images[selectedImage]->filterBlue();}));
     buttons.push_back(new Button(font, {660, 180}, {120, 30}, "Grey Scale", [&images, &selectedImage](){images[selectedImage]->greyScale();}));
     buttons.push_back(new Button(font, {660, 220}, {120, 30}, "Flip Horizontal", [&images, &selectedImage](){images[selectedImage]->flipHorizontal();}));
-    buttons.push_back(new Button(font, {660, 260}, {120, 30}, "Flip Vertical", [&images, &selectedImage](){images[selectedImage]->flipVertical();}));
+     buttons.push_back(new Button(font, {660, 260}, {120, 30}, "Flip Vertical", [&images, &selectedImage](){images[selectedImage]->flipVertical();}));
     buttons.push_back(new Button(font, {660, 300}, {120, 30}, "Advanced Feature 1", [&images, &selectedImage](){images[selectedImage]->advancedFeature1();}));
     buttons.push_back(new Button(font, {660, 340}, {120, 30}, "Advanced Feature 2", [&images, &selectedImage](){images[selectedImage]->advancedFeature2();}));
     buttons.push_back(new Button(font, {660, 380}, {120, 30}, "Advanced Feature 3", [&images, &selectedImage](){images[selectedImage]->advancedFeature3();}));
